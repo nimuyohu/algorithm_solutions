6d05174c52e7c2f4cf75f0ba2a00769d49e3637c
nimurarikuto <nimuyohu0105@gmail.com>
2021-06-29 08:51:33 +0900

    5.5 個数制限なし部分和問題

---
+++ 5_5.py
@@ -0,0 +1,15 @@
+n, W = map(int,input().split())
+a = list(map(int,input().split()))
+
+dp = [[False] * (W+1) for _ in range(n+1)]
+dp[0][0] = True
+
+for i in range(n):
+    for j in range(W+1):
+        if dp[i][j]:
+            dp[i+1][j] = True
+        if dp[i+1][j] and j+a[i] <= W:
+            dp[i+1][j+a[i]] = True
+
+if dp[n][W]:print('Yes')
+else:print('No')
